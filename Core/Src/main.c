40442b90ce54abf9be3ed39c905e7d201ecb546d
krisztinakiraly2 <krisztinakiraly2@gmail.com>
2024-10-02 18:59:00 +0200

    English keyboard done

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -55,8 +55,9 @@ extern USBD_HandleTypeDef hUsbDevice;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-void send_message(wchar_t*);
+void send_message(wchar_t*, wchar_t*);
 int* convert_message_hun(wchar_t*, int*, int*);
+int* convert_message_eng(wchar_t*, int*, int*);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -64,6 +65,9 @@ int* convert_message_hun(wchar_t*, int*, int*);
 GPIO_PinState led_set = GPIO_PIN_RESET;
 GPIO_PinState led_reset = GPIO_PIN_SET;
 GPIO_PinState is_pushed = GPIO_PIN_RESET;
+
+#define ENG L"english"
+#define HUN L"hungarian"
 
 uint16_t len = 8;
 uint8_t report[8] = {0};
@@ -106,8 +110,12 @@ int main(void)
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
   GPIO_PinState button;
   bool started=false;
-  wchar_t* message = L"ABCDEFGHIJKLMNOPQRSTUVWXYZ \nabcdefghijklmnopqrstuvwxyz \n0123456789 \nárvíztűrő tükörfúrógép \nÁRVÍZTŰRŐ TÜKÖRFÚRÓGÉP \n, . - / * + ? ! : \t @\n";
+  wchar_t* message_hun = L"ABCDEFGHIJKLMNOPQRSTUVWXYZ \nabcdefghijklmnopqrstuvwxyz \n0123456789 \nárvíztűrő tükörfúrógép \nÁRVÍZTŰRŐ TÜKÖRFÚRÓGÉP \n, . - / * + ? ! : \t @\n";
+  wchar_t* message_eng = L"ABCDEFGHIJKLMNOPQRSTUVWXYZ \nabcdefghijklmnopqrstuvwxyz \n0123456789 \n, . - / * + ? ! : \t @\n";
   int num = 0x50;
+  wchar_t del_str[2];
+  del_str[0] = 127;
+  del_str[1] = L'\0';
 
   bool test = false;
 
@@ -124,14 +132,12 @@ int main(void)
 	  {
 		  if(button == is_pushed)
 		  {
-			  send_message(message);
+			  /*send_message(message_hun,HUN);
+			  send_message(del_str,HUN);
+			  USBD_Delay(500);*/
+
+			  send_message(message_eng,ENG);
 			  USBD_Delay(500);
-
-			  wchar_t del_str[2];
-			  del_str[0] = 127;
-			  del_str[1] = L'\0';
-
-			  send_message(del_str);
 		  }
 	  }
 	  else
@@ -212,14 +218,17 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-void send_message(wchar_t* message)
+void send_message(wchar_t* message, wchar_t* lang)
 {
 	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, led_set);
 
 	int length = wcslen(message);
 	int t[length+1];
 	int is_special[length+1];
-	convert_message_hun(message, t, is_special);
+	if(wcscmp(lang,HUN)==1)
+		convert_message_hun(message, t, is_special);
+	else
+		convert_message_eng(message, t, is_special);
 	for(int i=0; i<length+1;++i)
 	{
 		if(i!=0 && t[i-1]=='\n')
@@ -336,6 +345,106 @@ int* convert_message_hun(wchar_t* message, int *t, int* is_special)
 		    case '@': is_special[i] = 0x40; t[i] = 0x19; break;
 
 		    case '0': t[i] = 0x35; break;
+		    case '1': t[i] = 0x1e; break;
+		    case '2': t[i] = 0x1f; break;
+		    case '3': t[i] = 0x20; break;
+		    case '4': t[i] = 0x21; break;
+		    case '5': t[i] = 0x22; break;
+		    case '6': t[i] = 0x23; break;
+		    case '7': t[i] = 0x24; break;
+		    case '8': t[i] = 0x25; break;
+		    case '9': t[i] = 0x26; break;
+
+		    default: t[i] = 0x00; break;
+		}
+	}
+
+	t[length] = 0x00;
+	is_special[length]= 0x00;
+
+	return t;
+}
+
+int* convert_message_eng(wchar_t* message, int *t, int* is_special)
+{
+	int length = wcslen(message);
+
+	for(int i=0; i<length; ++i)
+	{
+		is_special[i] = 0x00;
+		switch (message[i])
+		{
+		    case 'A': is_special[i] = 0x02;
+		    case 'a': t[i] = 0x04; break;
+		    case 'B': is_special[i] = 0x02;
+		    case 'b': t[i] = 0x05; break;
+		    case 'C': is_special[i] = 0x02;
+		    case 'c': t[i] = 0x06; break;
+		    case 'D': is_special[i] = 0x02;
+		    case 'd': t[i] = 0x07; break;
+		    case 'E': is_special[i] = 0x02;
+		    case 'e': t[i] = 0x08; break;
+		    case 'F': is_special[i] = 0x02;
+		    case 'f': t[i] = 0x09; break;
+		    case 'G': is_special[i] = 0x02;
+		    case 'g': t[i] = 0x0a; break;
+		    case 'H': is_special[i] = 0x02;
+		    case 'h': t[i] = 0x0b; break;
+		    case 'I': is_special[i] = 0x02;
+		    case 'i': t[i] = 0x0c; break;
+		    case 'J': is_special[i] = 0x02;
+		    case 'j': t[i] = 0x0d; break;
+		    case 'K': is_special[i] = 0x02;
+		    case 'k': t[i] = 0x0e; break;
+		    case 'L': is_special[i] = 0x02;
+		    case 'l': t[i] = 0x0f; break;
+		    case 'M': is_special[i] = 0x02;
+		    case 'm': t[i] = 0x10; break;
+		    case 'N': is_special[i] = 0x02;
+		    case 'n': t[i] = 0x11; break;
+		    case 'O': is_special[i] = 0x02;
+		    case 'o': t[i] = 0x12; break;
+		    case 'P': is_special[i] = 0x02;
+		    case 'p': t[i] = 0x13; break;
+		    case 'Q': is_special[i] = 0x02;
+		    case 'q': t[i] = 0x14; break;
+		    case 'R': is_special[i] = 0x02;
+		    case 'r': t[i] = 0x15; break;
+		    case 'S': is_special[i] = 0x02;
+		    case 's': t[i] = 0x16; break;
+		    case 'T': is_special[i] = 0x02;
+		    case 't': t[i] = 0x17; break;
+		    case 'U': is_special[i] = 0x02;
+		    case 'u': t[i] = 0x18; break;
+		    case 'V': is_special[i] = 0x02;
+		    case 'v': t[i] = 0x19; break;
+		    case 'W': is_special[i] = 0x02;
+		    case 'w': t[i] = 0x1a; break;
+		    case 'X': is_special[i] = 0x02;
+		    case 'x': t[i] = 0x1b; break;
+		    case 'Y': is_special[i] = 0x02;
+		    case 'y': t[i] = 0x1c; break;
+		    case 'Z': is_special[i] = 0x02;
+		    case 'z': t[i] = 0x1d; break;
+
+		    case '\n': t[i] = 0x28; break;
+		    case ' ': t[i] = 0x2c; break;
+		    case ',': t[i] = 0x36; break;
+		    case '.': t[i] = 0x37; break;
+		    case '-': t[i] = 0x2d; break;
+		    case '/': t[i] = 0x38; break;
+		    case '*': t[i] = 0x55; break;
+		    case '+': t[i] = 0x57; break;
+		    case '?': is_special[i] = 0x20; t[i] = 0x38; break;
+		    case '!': is_special[i] = 0x20; t[i] = 0x1e; break;
+		    case ':': is_special[i] = 0x20; t[i] = 0x33; break;
+		    case '\t': is_special[i] = 0x00; t[i] = 0x2b; break;
+		    case 127: is_special[i] = 0x00; t[i] = 0x2a; break; // delete
+
+		    // NO
+		    case '@': is_special[i] = 0x20; t[i] = 0x34; break;
+
+		    case '0': t[i] = 0x27; break;
 		    case '1': t[i] = 0x1e; break;
 		    case '2': t[i] = 0x1f; break;
 		    case '3': t[i] = 0x20; break;
